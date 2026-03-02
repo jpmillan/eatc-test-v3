83dd0192de4355db0e043af7379671910e6a8994
johnpaul.millan@slatergordon.com.au <johnpaul.millan@slatergordon.com.au>
2026-03-02 22:39:36 +1100

    add springboot shell main class

---
+++ src/main/java/com/eatclub/challenge/EatClubApplication.java
@@ -0,0 +1,13 @@
+package com.eatclub.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatClubApplication.class, args);
+    }
+
+}
